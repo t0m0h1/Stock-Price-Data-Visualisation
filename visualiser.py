35754e964a7a6a9b5df1e4f73758fa2adbb15a8f
t0m0h1 <thomashathawayhogg@gmail.com>
2025-02-19 19:26:00 +0000

    Remove visualiser.py file

--- visualiser.py
+++
@@ -1,9 +0,0 @@
-import yfinance as yf
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-symbol = 'AAPL' # Apple Inc, for example
-stock = yf.Ticker(symbol)
-data = stock.history(period='1y') # stock over the last year
-
